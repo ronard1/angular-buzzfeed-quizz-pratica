3d3f86d818bae6031bc506216081291b239f98d6
ronard1 <universia0503@gmail.com>
2024-02-06 05:03:56 -0500

    Creando las reglas del juego

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json"
 
 @Component({
   selector: 'app-quizz',
@@ -6,7 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.component.css'
 })
 
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+  
   titulo:string=""
 
   preguntas:any
@@ -19,4 +21,34 @@ export class QuizzComponent {
   preguntaMaxIndex:number = 0
 
   finalizado:boolean = false
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finalizado = false
+      this.titulo = quizz_questions.title
+      
+      this.preguntas = quizz_questions.questions
+      this.preguntaSeleccionada = this.preguntas[this.preguntaIndex]
+
+      this.preguntaIndex = 0
+      this.preguntaMaxIndex = this.preguntas.length
+
+      
+    }    
+  }
+
+  playerChoose(value:string){
+    this.respuestas.push();
+  }
+
+  async nextStep(){
+    this.preguntaIndex += 1
+
+    if (this.preguntaMaxIndex > this.preguntaIndex) {
+      this.preguntaSeleccionada = this.preguntas[this.preguntaIndex]
+    }
+    else{
+      this.finalizado = true
+    }
+  }
 }
