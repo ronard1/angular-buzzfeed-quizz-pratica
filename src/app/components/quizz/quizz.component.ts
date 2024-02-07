30995075d7645c92e83b1c244032579c27b35c6b
ronard1 <universia0503@gmail.com>
2024-02-07 04:21:05 -0500

    Calculando el resultado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import quizz_questions from "../../../assets/data/quizz_questions.json"
+import { ReturnStatement } from '@angular/compiler';
 
 @Component({
   selector: 'app-quizz',
@@ -38,7 +39,9 @@ export class QuizzComponent implements OnInit {
   }
 
   playerChoose(value:string){
-    this.respuestas.push();
+    this.respuestas.push(value);
+    this.nextStep();
+    //console.log(this.respuestas)
   }
 
   async nextStep(){
@@ -48,7 +51,27 @@ export class QuizzComponent implements OnInit {
       this.preguntaSeleccionada = this.preguntas[this.preguntaIndex]
     }
     else{
+      const finalRespuesta:string = await this.checkResult(this.respuestas)
       this.finalizado = true
+      this.respuestaSeleccionada = quizz_questions.results[finalRespuesta as keyof typeof quizz_questions.results]
+      //verificar opción ganadora
+      console.log(this.respuestas)
     }
   }
+
+  async checkResult(anwsers:string[]){
+    const result = anwsers.reduce((previous, current, i, arr)=>{
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous
+      }else{
+        return current
+      }
+  })
+
+  return result
+  }
+
 }
